9ae2bd446eff06c70951da1cf8eb6438b22fc80a
Shivani Parate <shivanidutt602@gmail.com>
2025-12-16 21:30:56 +0530

    changed the gemini model to gemini-1.0-pro

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ client_groq = Groq(api_key = st.secrets["GROQ_API_KEY"])
 def call_gemini(prompt):
     start = time.time()
 
-    model = genai.GenerativeModel("gemini-1.5-pro")
+    model = genai.GenerativeModel("gemini-1.0-pro")
     response = model.generate_content(prompt)
 
     end = time.time()
